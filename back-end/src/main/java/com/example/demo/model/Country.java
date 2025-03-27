dae1bd0dc22cda306cb4a087715bd1b1945fb1e5
Diogo Felipe <diogofelipe.dev@gmail.com>
2025-03-27 14:19:18 -0300

    Created Country model

---
+++ back-end/src/main/java/com/example/demo/model/Country.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "table_1")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Country {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(unique = true, nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String capital;
+
+  @Column(nullable = false)
+  private Integer population;
+
+  @Column(nullable = false)
+  private Float area;
+}
