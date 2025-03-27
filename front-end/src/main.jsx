538490c0180da85f9a07c254e68cc5f877b7c4a5
Diogo Felipe <diogofelipe.dev@gmail.com>
2025-03-27 14:43:27 -0300

    Removed the Strict Mode in the React Project

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,8 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+//I've removed the strict mode as this is a very simple project
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
